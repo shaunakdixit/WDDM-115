3d1478939f81c782dd2363f3f7cd92d029fdb4e2
Shaunak Dixit <dixitshaunak07@gmail.com>
2023-11-08 23:01:13 -0500

    changes to add more products in json list object

--- script.js
+++ script.js
@@ -16,7 +16,68 @@ const products = [
         img: "./img/air2.png",
       },
     ],
-  }
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      // {
+      //   code: "lightgray",
+      //   img: "./img/crater2.png",
+    
+      // },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
 ];
 
 let choosenProduct = products[0];
