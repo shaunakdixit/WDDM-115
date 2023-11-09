26b3f2a664df7d9bdf9213980605f2f03cc2da4c
Shaunak Dixit <dixitshaunak07@gmail.com>
2023-11-09 10:34:50 -0500

    Revert "changes to add comments in code"
    This reverts commit 2c9a5b33eb7a3155ccc1312975b867facdd947f2.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Selecting elements from the DOM
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
-// Product data
 const products = [
   {
     id: 1,
@@ -19,63 +17,115 @@ const products = [
       },
     ],
   },
-  // ... (similar data for other products)
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+    
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
 ];
 
-// Initial selected product
 let choosenProduct = products[0];
 
-// DOM elements for displaying current product information
 const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
 const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".size");
 
-// Event listener for menu items
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
-    // Change the current slide
+    //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
-    // Change the chosen product
+    //change the choosen product
     choosenProduct = products[index];
 
-    // Update texts of currentProduct
+    //change texts of currentProduct
     currentProductTitle.textContent = choosenProduct.title;
     currentProductPrice.textContent = "$" + choosenProduct.price;
     currentProductImg.src = choosenProduct.colors[0].img;
 
-    // Assign new colors
+    //assing new colors
     currentProductColors.forEach((color, index) => {
       color.style.backgroundColor = choosenProduct.colors[index].code;
     });
   });
 });
 
-// Event listener for color selection
 currentProductColors.forEach((color, index) => {
   color.addEventListener("click", () => {
     currentProductImg.src = choosenProduct.colors[index].img;
   });
 });
 
-// Event listener for size selection
 currentProductSizes.forEach((size, index) => {
   size.addEventListener("click", () => {
-    // Reset styles for all sizes
     currentProductSizes.forEach((size) => {
       size.style.backgroundColor = "white";
       size.style.color = "black";
     });
-
-    // Apply selected styles
     size.style.backgroundColor = "black";
     size.style.color = "white";
   });
 });
 
-// Event listener for displaying payment modal
 const productButton = document.querySelector(".productButton");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
@@ -84,12 +134,11 @@ productButton.addEventListener("click", () => {
   payment.style.display = "flex";
 });
 
-// Event listener for closing payment modal
 close.addEventListener("click", () => {
   payment.style.display = "none";
 });
 
-// Event listeners for color hover effects
+
 currentProductColors.forEach((color, index) => {
   color.addEventListener("mouseover", () => {
     color.style.border = "10px solid white"; // Change border on hover
@@ -100,7 +149,7 @@ currentProductColors.forEach((color, index) => {
   });
 });
 
-// Event listeners for image hover effects
+
 currentProductImg.addEventListener("mouseenter", () => {
   currentProductImg.style.border = "2px solid red"; // Change border on mouseenter
 });
@@ -108,3 +157,4 @@ currentProductImg.addEventListener("mouseenter", () => {
 currentProductImg.addEventListener("mouseleave", () => {
   currentProductImg.style.border = "none"; // Revert border on mouseleave
 });
+
