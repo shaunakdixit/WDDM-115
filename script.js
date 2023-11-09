f7f8be36bb1e9b358afdb91e9f9df9c3b4b4b5d3
Shaunak Dixit <dixitshaunak07@gmail.com>
2023-11-08 22:57:05 -0500

    changes to add products json list object

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  }
+];
