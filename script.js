584f2687e7abcc1a5359ee40809ba7603073e728
Shaunak Dixit <dixitshaunak07@gmail.com>
2023-11-22 20:55:47 -0500

    make code more modular and write each functionality within functions

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
-let products = []
-let choosenProduct
+
+let products = [];
+let chosenProduct;
 
 function fetchData() {
-
   fetch('https://raw.githubusercontent.com/shaunakdixit/WDDM-115/main/products.json')
     .then(response => response.json())
     .then(data => {
-      console.log(data)
       products = data;
-    }).then(() => {
-      choosenProduct = products[0];
+      if(products.length > 0) {
+        chosenProduct = products[0];
+        updateProductDisplay(chosenProduct);
+      }
     })
     .catch(error => console.error('Error fetching data:', error));
 }  
 
 fetchData();
-
-
 
 const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
@@ -26,42 +25,43 @@ const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".size");
 
+function updateProductDisplay(product) {
+    currentProductTitle.textContent = product.title;
+    currentProductPrice.textContent = `$${product.price}`;
+    currentProductImg.src = product.colors[0].img;
+}
+
+function updateColorSelection(colorIndex) {
+    currentProductImg.src = chosenProduct.colors[colorIndex].img;
+}
+
+function updateSizeSelection(selectedSize) {
+    currentProductSizes.forEach(size => {
+        size.style.backgroundColor = "white";
+        size.style.color = "black";
+    });
+    selectedSize.style.backgroundColor = "black";
+    selectedSize.style.color = "white";
+}
 
 menuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    //change the current slide
-    wrapper.style.transform = `translateX(${-100 * index}vw)`;
-
-    //change the choosen product
-    choosenProduct = products[index];
-
-    //change texts of currentProduct
-    currentProductTitle.textContent = choosenProduct.title;
-    currentProductPrice.textContent = "$" + choosenProduct.price;
-    currentProductImg.src = choosenProduct.colors[0].img;
-
-    //assing new colors
-    currentProductColors.forEach((color, index) => {
-      color.style.backgroundColor = choosenProduct.colors[index].code;
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+        chosenProduct = products[index];
+        updateProductDisplay(chosenProduct);
     });
-  });
 });
 
 currentProductColors.forEach((color, index) => {
-  color.addEventListener("click", () => {
-    currentProductImg.src = choosenProduct.colors[index].img;
-  });
+    color.addEventListener("click", () => {
+        updateColorSelection(index);
+    });
 });
 
-currentProductSizes.forEach((size, index) => {
-  size.addEventListener("click", () => {
-    currentProductSizes.forEach((size) => {
-      size.style.backgroundColor = "white";
-      size.style.color = "black";
+currentProductSizes.forEach(size => {
+    size.addEventListener("click", () => {
+        updateSizeSelection(size);
     });
-    size.style.backgroundColor = "black";
-    size.style.color = "white";
-  });
 });
 
 const productButton = document.querySelector(".productButton");
@@ -76,23 +76,20 @@ close.addEventListener("click", () => {
   payment.style.display = "none";
 });
 
-
 currentProductColors.forEach((color, index) => {
   color.addEventListener("mouseover", () => {
-    color.style.border = "10px solid white"; // Change border on hover
+    color.style.border = "10px solid white";
   });
 
   color.addEventListener("mouseout", () => {
-    color.style.border = "none"; // Revert border on mouseout
+    color.style.border = "none";
   });
 });
 
-
 currentProductImg.addEventListener("mouseenter", () => {
-  currentProductImg.style.border = "2px solid red"; // Change border on mouseenter
+  currentProductImg.style.border = "2px solid red";
 });
 
 currentProductImg.addEventListener("mouseleave", () => {
-  currentProductImg.style.border = "none"; // Revert border on mouseleave
+  currentProductImg.style.border = "none";
 });
-
